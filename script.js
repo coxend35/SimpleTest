0bebfe355a955194f60a687d0ebff3ef23cdaa31
User1 <User1@gmail.com>
2024-12-04 13:43:07 +0900

    Add listbox and modify OnBnClickedMsgBtn function

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ document.getElementById('changeTextButton').addEventListener('click', function()
     const content = document.querySelector('.content p');
     content.textContent = 'The text has been changed! Now, try committing this change in Git!';
 });
+
+document.getElementById('OnBnClickedMsgBtn').addEventListener('click', function() {
+    const content = document.querySelector('.content2 p');
+    content.innerHTML += '<br>Button clicked!'; // <br> 태그로 줄 바꿈을 추가
+});
