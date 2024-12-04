df2dcd9487df82f6128efd2b28d16e626de5a63f
User1 <User1@gmail.com>
2024-12-04 14:30:04 +0900

    Add OnBnClickedResetBtn function

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // JavaScript to change the text of the paragraph when button is clicked
-document.getElementById('changeTextButton').addEventListener('click', function() {
+document.getElementById('OnBnClickedMsgBtn').addEventListener('click', function() {
     const content = document.querySelector('.content p');
-    content.textContent = 'The text has been changed! Now, try committing this change in Git!';
+    content.innerHTML += '<br>Button clicked!'; // <br> 태그로 줄 바꿈을 추가
 });
 
-document.getElementById('OnBnClickedMsgBtn').addEventListener('click', function() {
-    const content = document.querySelector('.content2 p');
-    content.innerHTML += '<br>Button clicked!'; // <br> 태그로 줄 바꿈을 추가
+document.getElementById('OnBnClickedResetBtn').addEventListener('click', function() {
+    const content = document.querySelector('.content p');
+    content.textContent = 'This is a simple web page to practice Git and GitHub.';
 });
